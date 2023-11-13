9ec0237f1881f0db1aa60a573f0cc463159675f2
Daniel Pechersky <danny.pechersky@gmail.com>
2023-11-13 15:25:35 -0500

    Add some Option tests and remove noise

--- spec/option_spec.rb
+++ spec/option_spec.rb
@@ -1,51 +1,60 @@
 # frozen_string_literal: true
 
 RSpec.describe Linguadata::Option do
+  let(:some) { described_class::Some }
+  let(:none) { described_class::None }
+
   it "Option::Some and Option::None share the exact same methods" do
-    expect(described_class::Some.instance_methods).to match_array(described_class::None.instance_methods)
+    expect(some.instance_methods).to match_array(none.instance_methods)
   end
 
   it "returns value when necessary" do
-    expect(described_class::Some[1].value).to eq(1)
-    expect(described_class::Some[1].unwrap).to eq(1)
-    expect { described_class::None[].value }.to raise_error(Linguadata::Option::NoValueError)
-    expect { described_class::None[].unwrap }.to raise_error(Linguadata::Option::NoValueError)
+    expect(some[1].value).to eq(1)
+    expect(some[1].unwrap).to eq(1)
+    expect { none[].value }.to raise_error(described_class::NoValueError)
+    expect { none[].unwrap }.to raise_error(described_class::NoValueError)
   end
 
   it "map methods work as expected" do
-    expect(described_class::Some[1].map { |x| x + 1 }).to eq(described_class::Some[2])
-    expect(described_class::None[].map { |x| x + 1 }).to eq(described_class::None[])
+    expect(some[1].map { |x| x + 1 }).to eq(some[2])
+    expect(none[].map { |x| x + 1 }).to eq(none[])
   end
 
   it "some? and none? methods work as expected" do
-    expect(described_class::Some[1].some?).to eq(true)
-    expect(described_class::None[].some?).to eq(false)
+    expect(some[1].some?).to eq(true)
+    expect(none[].some?).to eq(false)
 
-    expect(described_class::Some[1].none?).to eq(false)
-    expect(described_class::None[].none?).to eq(true)
+    expect(some[1].none?).to eq(false)
+    expect(none[].none?).to eq(true)
   end
 
   it "unwrap_or_else and unwrap_or methods work as expected" do
-    expect(described_class::Some[1].unwrap_or_else { 2 }).to eq(1)
-    expect(described_class::None[].unwrap_or_else { 2 }).to eq(2)
+    expect(some[1].unwrap_or_else { 2 }).to eq(1)
+    expect(none[].unwrap_or_else { 2 }).to eq(2)
 
-    expect(described_class::Some[1].unwrap_or(2)).to eq(1)
-    expect(described_class::None[].unwrap_or(2)).to eq(2)
+    expect(some[1].unwrap_or(2)).to eq(1)
+    expect(none[].unwrap_or(2)).to eq(2)
   end
 
   it "and_then method works as expected" do
-    expect(described_class::Some[1].and_then { |x| described_class::Some[x + 1] }).to eq(described_class::Some[2])
-    expect(described_class::None[].and_then { |x| described_class::Some[x + 1] }).to eq(described_class::None[])
-    expect(described_class::Some[1].and_then { |x| described_class::None[] }).to eq(described_class::None[])
+    expect(some[1].and_then { |x| some[x + 1] }).to eq(some[2])
+    expect(none[].and_then { |x| some[x + 1] }).to eq(none[])
+    expect(some[1].and_then { |_| none[] }).to eq(none[])
+  end
+
+  it "filter method works as expected" do
+    expect(some[1].filter { |x| x > 0 }).to eq(some[1])
+    expect(some[1].filter { |x| x < 0 }).to eq(none[])
+    expect(none[].filter { |x| x > 0 }).to eq(none[])
   end
 
   describe "#from_nillable" do
     it "correctly converts nil to None" do
-      expect(described_class.from_nillable(nil)).to eq(described_class::None[])
+      expect(described_class.from_nillable(nil)).to eq(none[])
     end
 
     it "converts any other value to Some" do
-      expect(described_class.from_nillable(1)).to eq(described_class::Some[1])
+      expect(described_class.from_nillable(1)).to eq(some[1])
     end
   end
 end
