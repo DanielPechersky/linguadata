22b16cd83cf62aae67065b4d2fe3bc238ec5034d
Daniel Pechersky <danny.pechersky@gmail.com>
2023-11-13 15:27:11 -0500

    Remove noise from Result spec

--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe Linguadata::Result do
+  let(:success) { described_class::Success }
+  let(:failure) { described_class::Failure }
+
   it "Result::Success and Result::Failure share the exact same methods" do
-    expect(described_class::Success.instance_methods).to match_array(described_class::Failure.instance_methods)
+    expect(success.instance_methods).to match_array(failure.instance_methods)
   end
 
   it "returns value as option" do
-    expect(described_class::Success[1].success).to eq(Linguadata::Option::Some[1])
+    expect(success[1].success).to eq(Linguadata::Option::Some[1])
   end
 
   it "map methods work as expected" do
-    expect(described_class::Success[1].map { |x| x + 1 }).to eq(described_class::Success[2])
-    expect(described_class::Failure[1].map { |x| x + 1 }).to eq(described_class::Failure[1])
-    expect(described_class::Success[1].map_failure { |x| x + 1 }).to eq(described_class::Success[1])
-    expect(described_class::Failure[1].map_failure { |x| x + 1 }).to eq(described_class::Failure[2])
+    expect(success[1].map { |x| x + 1 }).to eq(success[2])
+    expect(failure[1].map { |x| x + 1 }).to eq(failure[1])
+    expect(success[1].map_failure { |x| x + 1 }).to eq(success[1])
+    expect(failure[1].map_failure { |x| x + 1 }).to eq(failure[2])
   end
 end
