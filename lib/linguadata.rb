2adecd5e77a4c16bf02f72d3b5fbcee2051ed7c3
Daniel Pechersky <danny.pechersky@gmail.com>
2023-11-13 20:23:01 -0800

    Rename _validate_block_presence to indicate it is private

--- lib/linguadata.rb
+++ lib/linguadata.rb
@@ -3,23 +3,12 @@
 require_relative "linguadata/version"
 
 module Linguadata
-  class RequiredBlockError < ArgumentError
-    def initialize(_msg = "A block was required but not passed")
-      super
-    end
-  end
-
-  def self.validate_block_presence(block)
-    raise RequiredBlockError if block.nil?
-    block
-  end
-
   module Option
     class Some < Data.define(:value)
       alias_method :unwrap, :value
 
       def map(&block)
-        Some[Linguadata.validate_block_presence(block).call(value)]
+        Some[Linguadata._validate_block_presence(block).call(value)]
       end
 
       def some? = true
@@ -27,18 +16,18 @@ module Linguadata
       def none? = false
 
       def unwrap_or_else(&block)
-        Linguadata.validate_block_presence(block)
+        Linguadata._validate_block_presence(block)
         value
       end
 
       def unwrap_or(_other) = value
 
       def and_then(&block)
-        Linguadata.validate_block_presence(block).call(value)
+        Linguadata._validate_block_presence(block).call(value)
       end
 
       def filter(&block)
-        if Linguadata.validate_block_presence(block).call(value)
+        if Linguadata._validate_block_presence(block).call(value)
           self
         else
           None[]
@@ -52,7 +41,7 @@ module Linguadata
       alias_method :unwrap, :value
 
       def map(&block)
-        Linguadata.validate_block_presence(block)
+        Linguadata._validate_block_presence(block)
         self
       end
 
@@ -61,18 +50,18 @@ module Linguadata
       def none? = true
 
       def unwrap_or_else(&block)
-        Linguadata.validate_block_presence(block).call
+        Linguadata._validate_block_presence(block).call
       end
 
       def unwrap_or(other) = other
 
       def and_then(&block)
-        Linguadata.validate_block_presence(block)
+        Linguadata._validate_block_presence(block)
         self
       end
 
       def filter(&block)
-        Linguadata.validate_block_presence(block)
+        Linguadata._validate_block_presence(block)
         self
       end
     end
@@ -109,20 +98,20 @@ module Linguadata
       def failure = Option::None[]
 
       def map(&block)
-        Success[Linguadata.validate_block_presence(block).call(value)]
+        Success[Linguadata._validate_block_presence(block).call(value)]
       end
 
       def map_failure(&block)
-        Linguadata.validate_block_presence(block)
+        Linguadata._validate_block_presence(block)
         self
       end
 
       def and_then(&block)
-        Linguadata.validate_block_presence(block).call(value)
+        Linguadata._validate_block_presence(block).call(value)
       end
 
       def or_else(&block)
-        Linguadata.validate_block_presence(block)
+        Linguadata._validate_block_presence(block)
         self
       end
     end
@@ -142,21 +131,21 @@ module Linguadata
       def failure = Option::Some[error]
 
       def map(&block)
-        Linguadata.validate_block_presence(block)
+        Linguadata._validate_block_presence(block)
         self
       end
 
       def map_failure(&block)
-        Failure[Linguadata.validate_block_presence(block).call(error)]
+        Failure[Linguadata._validate_block_presence(block).call(error)]
       end
 
       def and_then(&block)
-        Linguadata.validate_block_presence(block)
+        Linguadata._validate_block_presence(block)
         self
       end
 
       def or_else(&block)
-        Linguadata.validate_block_presence(block).call(error)
+        Linguadata._validate_block_presence(block).call(error)
       end
     end
 
@@ -171,5 +160,16 @@ module Linguadata
         super
       end
     end
+  end
+
+  class RequiredBlockError < ArgumentError
+    def initialize(_msg = "A block was required but not passed")
+      super
+    end
+  end
+
+  def self._validate_block_presence(block)
+    raise RequiredBlockError if block.nil?
+    block
   end
 end
